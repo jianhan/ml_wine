e8729fe71e646052e9c0ec874cd8945b4a3008bd
Jian Han <jianhan@mail.com>
2018-07-14 20:14:01 +1000

    [refactor] Created a new dataset in csv, columns rename

--- main.py
+++ main.py
@@ -3,25 +3,63 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
+
 pd.options.mode.chained_assignment = None  # default='warn'
 from sklearn.pipeline import make_pipeline
 from sklearn.ensemble import RandomForestRegressor
 from sklearn import preprocessing
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 
-'''Data Retrieva'''
-# read csv
-df = pd.read_csv('epl.csv')
-df = df.dropna(subset=['FTHG', 'FTAG'])
-df['YEAR'] = pd.to_datetime(df['Date']).map(lambda x: x.year)
-df['MONTH'] = pd.to_datetime(df['Date']).map(lambda x: x.month)
-df['DAY'] = pd.to_datetime(df['Date']).map(lambda x: x.day)
+def cleanup_column_names(df,rename_dict={},do_inplace=True):
+    """This function renames columns of a pandas dataframe
+    It converts column names to snake case if rename_dict is not passed.
+    Args:
+    rename_dict (dict): keys represent old column names and values point to
+    newer ones
+    do_inplace (bool): flag to update existing dataframe or return a new one
+    Returns:
+    pandas dataframe if do_inplace is set to False, None otherwise
+    """
+    if not rename_dict:
+        return df.rename(columns={col: col.lower().replace(' ','_')
+        for col in df.columns.values.tolist()},inplace=do_inplace)
+    else:
+        return df.rename(columns=rename_dict,inplace=do_inplace)
+
+def dataCollection():
+    # read csv
+    df = pd.read_csv('epl.csv')
+    df = df.dropna(subset=['FTHG', 'FTAG'])
+    df['YEAR'] = pd.to_datetime(df['Date']).map(lambda x: x.year)
+    df['MONTH'] = pd.to_datetime(df['Date']).map(lambda x: x.month)
+    df['DAY'] = pd.to_datetime(df['Date']).map(lambda x: x.day)
+    cleanup_column_names(df)
+    print("Number of rows::", df.shape[0])
+    print("Number of columns::", df.shape[1])
+    print("Column Names::", df.columns.values.tolist())
+    print("Column Data Types::\n", df.dtypes)
+    print("Columns with Missing Values::", df.columns[df.isnull().any()].tolist())
+    print("Number of rows with Missing Values::", len(pd.isnull(df).any(1).nonzero()[0].tolist()))
+    print("Sample Indices with missing data::", pd.isnull(df).any(1).nonzero()[0].tolist()[0:5])
+    print("General Stats::")
+    print(df.info())
+    print("Summary Stats::")
+    print(df.describe())
+    return df
+
+def main():
+    df = dataCollection()
+
+if __name__ == '__main__':
+    main()
+
+exit(0)
+
 
 '''Data Preparation'''
 
@@ -30,8 +68,6 @@ feature_names = ['YEAR', 'MONTH', 'DAY', 'HomeTeam', 'AwayTeam', 'B365H', 'B365D
 training_features = df[feature_names]
 outcome_name = ['FTR']
 outcome_labels = df[outcome_name]
-
-
 
 gle = LabelEncoder()
 
@@ -54,7 +90,6 @@ print(away_team_mappings)
 training_features = training_features.drop('HomeTeam', axis=1)
 training_features = training_features.drop('AwayTeam', axis=1)
 
-
 # 4. Split data into training and test sets
 
 
@@ -70,8 +105,6 @@ sc = StandardScaler()
 X_train_std = sc.fit_transform(X_train)
 X_test_std = sc.transform(X_test)
 
-
-
 # 5. Declare data preprocessing steps
 pipeline = make_pipeline(preprocessing.StandardScaler(),
                          RandomForestRegressor(n_estimators=100))
@@ -83,17 +116,16 @@ hyperparameters = {'randomforestregressor__max_features': ['auto', 'sqrt', 'log2
 # 7. Tune model using cross-validation pipeline
 
 import numpy as np
+
 #
 # fit the model
 lr = LogisticRegression()
 model = lr.fit(X_train, y_train.values.ravel())
 
-
 pred = model.predict(X_test)
 print('Accuracy:', float(accuracy_score(y_test, pred)) * 100, '%')
 print('Classification Stats:')
 print(classification_report(y_test, pred))
-
 
 dct = DecisionTreeClassifier()
 dctModel = dct.fit(X_train, y_train.values.ravel())
@@ -102,7 +134,6 @@ print('Accuracy:', float(accuracy_score(y_test, dctPred)) * 100, '%')
 print('Classification Stats:')
 print(classification_report(y_test, dctPred))
 
-
 knc = KNeighborsClassifier()
 kncModel = knc.fit(X_train, y_train.values.ravel())
 kncPred = kncModel.predict(X_test)
@@ -110,11 +141,9 @@ print('Accuracy:', float(accuracy_score(y_test, kncPred)) * 100, '%')
 print('Classification Stats:')
 print(classification_report(y_test, kncPred))
 
-
 gnb = GaussianNB()
 gnbModel = gnb.fit(X_train, y_train.values.ravel())
 gnbPred = gnbModel.predict(X_test)
 print('Accuracy:', float(accuracy_score(y_test, gnbPred)) * 100, '%')
 print('Classification Stats:')
 print(classification_report(y_test, gnbPred))
-
