522149a043727c925f8cc466499ed4e49e3e44b8
Jian Han <jianhan@mail.com>
2018-07-14 12:31:30 +1000

    [feat] Model and evaluation done

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ df = df.dropna(subset=['FTHG', 'FTAG'])
 '''Data Preparation'''
 
 """Feature Extraction and Engineering"""
-feature_names = ['Date', 'HomeTeam', 'AwayTeam', 'FTHG', 'FTAG']
+feature_names = ['HomeTeam', 'AwayTeam', 'FTHG', 'FTAG']
 training_features = df[feature_names]
 outcome_name = ['FTR']
 outcome_labels = df[outcome_name]
@@ -29,5 +29,28 @@ ss.fit(training_features[numeric_feature_names])
 
 # scale numeric features now
 training_features[numeric_feature_names] = ss.transform(training_features[numeric_feature_names])
+training_features = pd.get_dummies(training_features,columns=categoricial_feature_names)
 
-print(training_features)
+# get list of new categorical features
+categorical_engineered_features = list(set(training_features.columns) - set(numeric_feature_names))
+
+'''Modeling'''
+"""We will now build a simple classification (supervised) model based on our feature set by using the logistic
+regression algorithm"""
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+# fit the model
+lr = LogisticRegression()
+model = lr.fit(training_features, np.array(outcome_labels['FTR']))
+# view model parameters
+
+'''Model Evaluation'''
+# simple evaluation on training data
+pred_labels = model.predict(training_features)
+actual_labels = np.array(outcome_labels['FTR'])
+# evaluate model performance
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+print('Accuracy:', float(accuracy_score(actual_labels,pred_labels))*100, '%')
+print('Classification Stats:')
+print(classification_report(actual_labels, pred_labels))
